d02a86001027546dcee900705f477bd803102fe3
jacksonlee-civis <jacksonlee@civisanalytics.com>
2023-04-24 14:23:07 -0400

    DOC add example code for graph with no edges

--- docs/source/more_examples.rst
+++ docs/source/more_examples.rst
@@ -30,6 +30,21 @@ to select which nodes to execute instead and/or supply the arguments to whicheve
    :language: python
    :emphasize-lines: 34
 
+Graph with Nodes only and No Edges
+----------------------------------
+
+It is possible to execute a graph with nodes only and no edges.
+In this case,
+if ``start_nodes`` at :func:`~async_graph_data_flow.AsyncExecutor.execute` are not specified (see above),
+then *all* nodes are start nodes (because all nodes don't have an incoming edge),
+and therefore all nodes will execute concurrently upon graph execution.
+The start nodes and their arguments at the beginning of the graph execution are available at
+:attr:`~async_graph_data_flow.AsyncExecutor.start_nodes` of :func:`~async_graph_data_flow.AsyncExecutor.execute`.
+
+.. literalinclude:: ../../examples/graph_with_no_edges.py
+   :language: python
+   :emphasize-lines: 30,39,48
+
 Data Flow Statistics and Logging
 --------------------------------
 
---
+++ examples/graph_with_no_edges.py
@@ -0,0 +1,48 @@
+import asyncio
+import time
+
+from async_graph_data_flow import AsyncExecutor, AsyncGraph
+
+
+async def node1():
+    print("from node1")
+    await asyncio.sleep(2)
+    yield
+
+
+async def node2():
+    print("from node2")
+    await asyncio.sleep(2)
+    yield
+
+
+async def node3():
+    print("from node3")
+    await asyncio.sleep(2)
+    yield
+
+
+if __name__ == "__main__":
+    graph = AsyncGraph()
+    graph.add_node(node1)
+    graph.add_node(node2)
+    graph.add_node(node3)
+    # Nodes are added, but there are no edges.
+
+    print(f"Graph: {graph.nodes_to_edges}")
+    executor = AsyncExecutor(graph)
+
+    t1 = time.time()
+    executor.execute()
+    t2 = time.time()
+    print("execution time:", t2 - t1)
+    print(f"start nodes:", executor.start_nodes)
+
+    # Output:
+    # -------
+    # Graph: {'node1': set(), 'node2': set(), 'node3': set()}
+    # from node1
+    # from node2
+    # from node3
+    # execution time: 2.0039637088775635
+    # start nodes: {'node3': (), 'node1': (), 'node2': ()}
